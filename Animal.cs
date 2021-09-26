8d32affbe37e3540ed1ebe4e22225ea2a3434439
Павел Семейко <pawel.semeiko@yandex.ru>
2021-09-26 15:52:47 +0300

    Добавьте файлы проекта.

---
+++ Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yrok1
+{
+    class Animal
+    {
+        public string Name { get; set; }  // свойство
+        public virtual void Golos()  // метод
+        {
+            Console.WriteLine(" Клич животного");
+        }
+    }
+    class Cat : Animal 
+    {
+        public override void Golos()
+        {
+            Console.WriteLine(" мяу мяу мяу");
+        }
+
+    }
+    class Dog : Animal
+    {
+        public override void Golos()
+        {
+            Console.WriteLine(" Гав гав гав");
+        }
+    }
+    class Rat : Animal
+    {
+        public override void Golos()
+        {
+            Console.WriteLine(" писк писк писк");
+        }
+    }
+    class Boozer : Animal
+    {
+        public override void Golos()
+        {
+            Console.WriteLine(" Э пс... е шо ?");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Yrok1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[4];
+            Cat cat1 = new Cat();
+            cat1.Name = "Барсик";
+            Dog dog1 = new Dog();
+            dog1.Name = "Мухтар";
+            Rat rat1 = new Rat();
+            rat1.Name = "Саймон";
+            Boozer boozer1 = new Boozer();
+            boozer1.Name = "Дядя Гриша";
+
+            animals[0] = cat1;
+            animals[1] = dog1;
+            animals[2] = rat1;
+            animals[3] = boozer1;
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(" Животное " + animals[i] + " говорит : ");
+                animals[i].Golos();
+            }
+
+        }
+    }
+}
